467dd623127ce172c2e423e7368896c0f825230a
Jeong gi seok <wjdrltjr5@naver.com>
2024-07-27 12:07:45 +0900

    Feat : outgoingReq state pendingToReady temp

--- src/main/java/com/seahere/backend/outgoing/controller/response/OutgoingReqMockDto.java
+++ src/main/java/com/seahere/backend/outgoing/controller/response/OutgoingReqMockDto.java
@@ -1,21 +1,35 @@
 package com.seahere.backend.outgoing.controller.response;
 
+import com.seahere.backend.outgoing.entity.OutgoingState;
 import lombok.Getter;
+import lombok.ToString;
 
 import java.time.LocalDate;
 @Getter
+@ToString
 public class OutgoingReqMockDto {
     private Long outgoingId;
     private String customerName;
     private String title;
+    private OutgoingState state;
     private String status;
     private LocalDate outgoingDate;
 
-    public OutgoingReqMockDto(Long outgoingId, String customerName, String title, String status) {
+    public OutgoingReqMockDto(Long outgoingId, String customerName, String title, OutgoingState state) {
         this.outgoingId = outgoingId;
         this.customerName = customerName;
+        this.status = state.printState();
         this.title = title;
-        this.status = status;
+        this.state = state;
         this.outgoingDate = LocalDate.now();
+    }
+
+    public void setOutgoingStateToReady(){
+        this.state = OutgoingState.ready;
+        this.status = state.printState();
+    }
+    public void setOutgoingStateToReject(){
+        this.state = OutgoingState.reject;
+        this.status = state.printState();
     }
 }
