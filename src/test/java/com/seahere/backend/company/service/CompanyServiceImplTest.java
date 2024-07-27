e718daa297dd86c92f185c85e6dbff0f841c013a
kimdodo97 <eh4536@naver.com>
2024-07-27 21:34:08 +0900

    Test : 회사 서비스 저장/ID 조회 테스트 추가

---
+++ src/test/java/com/seahere/backend/company/service/CompanyServiceImplTest.java
@@ -0,0 +1,102 @@
+package com.seahere.backend.company.service;
+
+import com.seahere.backend.common.entity.Address;
+import com.seahere.backend.company.entity.CompanyEntity;
+import com.seahere.backend.company.exception.CompanyNotFound;
+import com.seahere.backend.company.repository.CompanyRepository;
+import com.seahere.backend.company.request.CompanyCreateReq;
+import com.seahere.backend.company.response.CompanyResponse;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class CompanyServiceImplTest {
+    @Autowired
+    CompanyService companyService;
+
+    @Autowired
+    CompanyRepository companyRepository;
+    
+    @Test
+    @DisplayName("CompanyCreateReq를 통해서 회사 등록이 가능하다.")
+    void test1() throws Exception {
+        //given
+        Address address = Address.builder()
+                .postCode("12345")
+                .mainAddress("부산광역시")
+                .subAddress("스파로스 아카데미")
+                .build();
+
+        CompanyCreateReq companyCreateReq = CompanyCreateReq.builder()
+                .companyName("여보소수산")
+                .registrationNumber("123456")
+                .address(address)
+                .build();
+        //when
+        Long savedId = companyService.save(companyCreateReq);
+        CompanyEntity result = companyRepository.findById(savedId).orElseThrow(CompanyNotFound::new);
+
+        //then
+        assertEquals(savedId,result.getId());
+        assertEquals("여보소수산",result.getCompanyName());
+        assertEquals("123456",result.getRegistrationNumber());
+    }
+    
+    @Test
+    @DisplayName("Company ID로 회사 정보 조회가 가능하다")
+    void test2() throws Exception {
+        //given
+        Address address = Address.builder()
+                .postCode("12345")
+                .mainAddress("부산광역시")
+                .subAddress("스파로스 아카데미")
+                .build();
+
+        CompanyEntity company = CompanyEntity.builder()
+                .id(1L)
+                .companyName("여보소수산")
+                .registrationNumber("123456")
+                .address(address)
+                .build();
+
+        companyRepository.save(company);
+        //when
+
+        CompanyResponse result = companyService.getCompanyById(1L);
+
+        //then
+        assertEquals(result.getCompanyName(),"여보소수산");
+        assertEquals(result.getRegistrationNumber(),"123456");
+    }
+
+    @Test
+    @DisplayName("사업자 번호로 회사 정보 조회가 가능하다")
+    void test3() throws Exception {
+        //given
+        Address address = Address.builder()
+                .postCode("12345")
+                .mainAddress("부산광역시")
+                .subAddress("스파로스 아카데미")
+                .build();
+
+        CompanyEntity company = CompanyEntity.builder()
+                .id(1L)
+                .companyName("여보소수산")
+                .registrationNumber("123456")
+                .address(address)
+                .build();
+
+        companyRepository.save(company);
+        //when
+
+        CompanyResponse result = companyService.getCompanyByRegNumber("123456");
+
+        //then
+        assertEquals(result.getCompanyName(),"여보소수산");
+        assertEquals(result.getRegistrationNumber(),"123456");
+    }
+}
