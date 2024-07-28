bbde58b902fd69010e377aeb734c90a26f60c55d
kimdodo97 <eh4536@naver.com>
2024-07-28 20:15:11 +0900

    Fix : 회사 엔티티 테이블 이름 설정 추가

--- src/main/java/com/seahere/backend/company/entity/CompanyEntity.java
+++ src/main/java/com/seahere/backend/company/entity/CompanyEntity.java
@@ -12,6 +12,7 @@ import javax.persistence.*;
 @Entity
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(name = "companies")
 public class CompanyEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
