026f62b24326cd2af253dc205b14ad6e020b0363
kimdodo97 <eh4536@naver.com>
2024-07-29 15:25:09 +0900

    Fix : 회원가입 완료 시 로그인 페이지 URI 전달

--- src/main/java/com/seahere/backend/user/controller/UserController.java
+++ src/main/java/com/seahere/backend/user/controller/UserController.java
@@ -8,14 +8,16 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.net.URI;
+
 @RestController
 @RequiredArgsConstructor
 public class UserController {
     private final UserService userService;
 
     @PostMapping("/users/customer")
-    public ResponseEntity<Long> customerAdd(@RequestBody CustomerSignupReq customerSignupReq){
-        Long savedId = userService.signupCustomer(customerSignupReq);
-        return ResponseEntity.ok(savedId);
+    public ResponseEntity<Void> customerAdd(@RequestBody CustomerSignupReq customerSignupReq){
+        userService.signupCustomer(customerSignupReq);
+        return ResponseEntity.created(URI.create("/login")).build();
     }
 }
