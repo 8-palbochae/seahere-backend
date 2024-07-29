faa0ecfa44cd99362321189b7a06712af33db511
kimdodo97 <eh4536@naver.com>
2024-07-30 01:16:00 +0900

    Feat : CEO 회원가입 서비스 로직 구현

--- src/main/java/com/seahere/backend/user/service/UserServiceImpl.java
+++ src/main/java/com/seahere/backend/user/service/UserServiceImpl.java
@@ -1,9 +1,13 @@
 package com.seahere.backend.user.service;
 
 import com.seahere.backend.common.entity.Role;
+import com.seahere.backend.company.entity.CompanyEntity;
+import com.seahere.backend.company.exception.CompanyNotFound;
+import com.seahere.backend.company.repository.CompanyRepository;
 import com.seahere.backend.user.domain.UserEntity;
 import com.seahere.backend.user.repository.UserRepository;
 import com.seahere.backend.user.request.BrokerSignupReq;
+import com.seahere.backend.user.request.CeoSignupReq;
 import com.seahere.backend.user.request.CustomerSignupReq;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -15,6 +19,7 @@ import org.springframework.transaction.annotation.Transactional;
 @Transactional
 public class UserServiceImpl implements UserService{
     private final UserRepository userRepository;
+    private final CompanyRepository companyRepository;
     private final PasswordEncoder passwordEncoder;
 
     @Override
@@ -29,6 +34,19 @@ public class UserServiceImpl implements UserService{
     public Long signupBroker(BrokerSignupReq brokerSignupReq) {
         UserEntity user = brokerSignupReq.to();
         user.passwordEncode(passwordEncoder);
+        userRepository.save(user);
+        return user.getId();
+    }
+
+    @Override
+    public Long signupCeo(CeoSignupReq ceoSignupReq) {
+        UserEntity user = ceoSignupReq.to();
+        user.passwordEncode(passwordEncoder);
+
+        CompanyEntity company = companyRepository.findById(ceoSignupReq.getCompanyId())
+                .orElseThrow(CompanyNotFound::new);
+        user.updateCompany(company);
+
         userRepository.save(user);
         return user.getId();
     }
