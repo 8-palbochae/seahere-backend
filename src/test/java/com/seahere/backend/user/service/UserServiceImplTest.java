a64cd2e7c508ada4749808c0b7a16e055b5d7798
kimdodo97 <eh4536@naver.com>
2024-07-30 01:01:06 +0900

    Test : 브로커 회원가입 테스트 추가

--- src/test/java/com/seahere/backend/user/service/UserServiceImplTest.java
+++ src/test/java/com/seahere/backend/user/service/UserServiceImplTest.java
@@ -3,8 +3,10 @@ package com.seahere.backend.user.service;
 import com.seahere.backend.common.entity.Address;
 import com.seahere.backend.common.entity.Role;
 import com.seahere.backend.user.domain.UserEntity;
+import com.seahere.backend.user.domain.UserStatus;
 import com.seahere.backend.user.exception.UserNotFound;
 import com.seahere.backend.user.repository.UserRepository;
+import com.seahere.backend.user.request.BrokerSignupReq;
 import com.seahere.backend.user.request.CustomerSignupReq;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -50,6 +52,34 @@ class UserServiceImplTest{
         assertEquals(findUser.getEmail(),signupReq.getEmail());
         assertTrue(encoder.matches("1234", findUser.getPassword()));
         assertEquals(findUser.getRole(), Role.CUSTOMER);
+    }
+
+    @Test
+    @DisplayName("BrokerSignup 클래스를 통해서  브로커 일반 회원 가입이 가능하고 상태는 PENDING이다")
+    void test2() throws Exception{
+        //given
+        Address address = Address.builder()
+                .postCode("12345")
+                .mainAddress("메인 주소")
+                .subAddress("상세 주소")
+                .build();
+
+        BrokerSignupReq signupReq = BrokerSignupReq.builder()
+                .email("test@test.com")
+                .username("여보소")
+                .password("1234")
+                .address(address)
+                .build();
+        //when
+        Long userId = userService.signupBroker(signupReq);
+
+        //then
+        UserEntity findUser = userRepository.findById(userId).orElseThrow(UserNotFound::new);
+        assertEquals(findUser.getId(),userId);
+        assertEquals(findUser.getEmail(),signupReq.getEmail());
+        assertTrue(encoder.matches("1234", findUser.getPassword()));
+        assertEquals(findUser.getRole(), Role.EMPLOYEE);
+        assertEquals(findUser.getStatus(), UserStatus.PENDING);
     }
 }
 
