3a97fea3aa30cda91ced95bd447e05c261bccca3
kimdodo97 <eh4536@naver.com>
2024-07-30 16:12:56 +0900

    Feat : 소셜 회원 가입 서비스 로직 추가

--- src/main/java/com/seahere/backend/user/service/UserService.java
+++ src/main/java/com/seahere/backend/user/service/UserService.java
@@ -3,11 +3,12 @@ package com.seahere.backend.user.service;
 import com.seahere.backend.user.request.BrokerSignupReq;
 import com.seahere.backend.user.request.CeoSignupReq;
 import com.seahere.backend.user.request.CustomerSignupReq;
+import com.seahere.backend.user.request.OAuthSignupReq;
 
 public interface UserService {
     Long signupCustomer(CustomerSignupReq customerSignupReq);
     Long signupBroker(BrokerSignupReq brokerSignupReq);
-
     Long signupCeo(CeoSignupReq ceoSignupReq);
+    Long signupOauth(OAuthSignupReq oauthSignupReq);
     Boolean validateEmail(String email);
 }
