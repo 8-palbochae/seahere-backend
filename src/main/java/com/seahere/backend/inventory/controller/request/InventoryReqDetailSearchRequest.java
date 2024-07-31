120b27de8a25be1e77ecb560b952a6516248dd0d
정기석(Gi Seok Jeong) <48271492+wjdrltjr5@users.noreply.github.com>
2024-07-31 14:02:42 +0900

    outgoing 엔티티 UserName -> userEntity로 변경 (#41)
    * Feat : outgoingEntity add userEntity
    
    * Feat : outgoing entity userentity적용

--- src/main/java/com/seahere/backend/inventory/controller/request/InventoryReqDetailSearchRequest.java
+++ src/main/java/com/seahere/backend/inventory/controller/request/InventoryReqDetailSearchRequest.java
@@ -9,7 +9,9 @@ import lombok.*;
 @Builder
 public class InventoryReqDetailSearchRequest {
     private Long companyId;
+    @Builder.Default
     private int size = 10;
+    @Builder.Default
     private int page = 0;
     private String name;
     private String category;
--- src/test/resources/sql/outgoing-service-test.sql
+++ src/test/resources/sql/outgoing-service-test.sql
@@ -1,10 +1,17 @@
+delete from outgoing_detail;
+delete from outgoing;
+delete from company;
+delete from users;
 INSERT INTO company (company_id) values (1);
-INSERT INTO outgoing (outgoing_id, company_id, outgoing_date, outgoing_state, partial_outgoing,customer_name) VALUES
-    (1, 1, '2024-7-20', 'PENDING', true,'아리랑'),
-    (2, 1, '2024-7-28', 'READY', true,'스리랑'),
-    (3, 1, '2024-7-28', 'PENDING', true,'스리랑'),
-    (4, 1, '2024-7-28', 'PENDING', true,'스리랑'),
-    (5, 1, '2024-7-28', 'READY', true,'스리랑');
+insert into USERS(user_id,username) values(1,'아리랑'),
+                                          (2,'스리랑');
+
+INSERT INTO outgoing (outgoing_id, company_id, outgoing_date, outgoing_state, partial_outgoing,user_id) VALUES
+    (1, 1, '2024-7-20', 'PENDING', true,1),
+    (2, 1, '2024-7-28', 'READY', true,2),
+    (3, 1, '2024-7-28', 'PENDING', true,2),
+    (4, 1, '2024-7-28', 'PENDING', true,2),
+    (5, 1, '2024-7-28', 'READY', true,2);
 
 INSERT INTO outgoing_detail (detail_id, price, product_name, outgoing_id, quantity) values (1,10000,'광어',1,20),
                                                                                            (2,10000,'광어',3,20);
