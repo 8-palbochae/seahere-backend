dca51ee3046a347db57c3fe2ff1c9d62fbed31c8
kimdodo97 <eh4536@naver.com>
2024-07-31 15:07:37 +0900

    Fix : 인증 토큰 발급 헤더 발급으로 변경

--- src/main/java/com/seahere/backend/auth/controller/AuthController.java
+++ src/main/java/com/seahere/backend/auth/controller/AuthController.java
@@ -2,12 +2,12 @@ package com.seahere.backend.auth.controller;
 
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
-import com.seahere.backend.auth.controller.response.AuthenticationRes;
+import com.seahere.backend.auth.jwt.exception.ValidateTokenException;
 import com.seahere.backend.auth.jwt.util.CookieUtil;
 import lombok.extern.slf4j.Slf4j;
-import org.apache.coyote.Response;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.http.MediaType;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -19,8 +19,9 @@ import javax.servlet.http.HttpServletRequest;
 public class AuthController {
     @Value("${jwt.secretKey}")
     private String secretKey;
+
     @GetMapping("/authentication/protected")
-    public ResponseEntity<AuthenticationRes> authenticationTokenGet(HttpServletRequest request){
+    public ResponseEntity<Void> authenticationTokenGet(HttpServletRequest request) {
         String accessToken = CookieUtil.getAccessTokenFromCookies(request);
         String refreshToken = CookieUtil.getRefreshTokenFromCookie(request);
 
@@ -28,19 +29,14 @@ public class AuthController {
             JWT.require(Algorithm.HMAC512(secretKey)).build().verify(accessToken);
             JWT.require(Algorithm.HMAC512(secretKey)).build().verify(refreshToken);
 
-            AuthenticationRes authenticationRes = AuthenticationRes.builder()
-                    .accessToken(accessToken)
-                    .refreshToken(refreshToken)
-                    .build();
+            HttpHeaders headers = new HttpHeaders();
+            headers.set("Authorization", accessToken);
+            headers.set("Authorization-Refresh", refreshToken);
 
-            return ResponseEntity
-                    .ok()
-                    .contentType(MediaType.APPLICATION_JSON)
-                    .body(authenticationRes);
+            return new ResponseEntity<>(headers, HttpStatus.OK);
 
         } catch (Exception e) {
-            return ResponseEntity.status(401).body(new AuthenticationRes());
+            throw new ValidateTokenException();
         }
-
     }
 }
