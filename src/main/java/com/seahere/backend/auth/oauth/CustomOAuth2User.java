54185f05d87eba07ab7b824861b7932e8b8e62bb
kimdodo97 <eh4536@naver.com>
2024-08-02 02:06:38 +0900

    Feat : 소셜 사용자 상태 정보 추가

--- src/main/java/com/seahere/backend/auth/oauth/CustomOAuth2User.java
+++ src/main/java/com/seahere/backend/auth/oauth/CustomOAuth2User.java
@@ -1,6 +1,7 @@
 package com.seahere.backend.auth.oauth;
 
 import com.seahere.backend.common.entity.Role;
+import com.seahere.backend.user.domain.UserStatus;
 import lombok.Getter;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.oauth2.core.user.DefaultOAuth2User;
@@ -13,12 +14,13 @@ public class CustomOAuth2User extends DefaultOAuth2User {
 
     private String email;
     private Role role;
-
+    private UserStatus status;
     public CustomOAuth2User(Collection<? extends GrantedAuthority> authorities,
                             Map<String, Object> attributes, String nameAttributeKey,
-                            String email, Role role) {
+                            String email, Role role, UserStatus status) {
         super(authorities, attributes, nameAttributeKey);
         this.email = email;
         this.role = role;
+        this.status = status;
     }
 }
