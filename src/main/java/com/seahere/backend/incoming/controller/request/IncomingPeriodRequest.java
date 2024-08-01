8f6e57c20f93d86bbf6b7dc8d51b4ba6c8c49703
woneveryady <woneveryday@naver.com>
2024-08-01 22:01:27 +0900

    ㄴãstartDate, endDate 요ìendDate 받기

---
+++ src/main/java/com/seahere/backend/incoming/controller/request/IncomingPeriodRequest.java
@@ -0,0 +1,22 @@
+package com.seahere.backend.incoming.controller.request;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.extern.slf4j.Slf4j;
+import net.bytebuddy.asm.Advice;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import java.time.LocalDate;
+
+@Getter
+@Setter
+@Slf4j
+public class IncomingPeriodRequest {
+
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
+    private LocalDate startDate = LocalDate.now();
+    private LocalDate endDate = LocalDate.now();
+
+
+
+}
