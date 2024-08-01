020d79f6257d5128bd7c1863bf69dc802a43be9d
kimdodo97 <eh4536@naver.com>
2024-08-02 00:54:48 +0900

    Feat : 브로커(직원/CEO) 소셜 로그인 시 회사 정보 함께 응답

--- src/main/java/com/seahere/backend/auth/controller/AuthController.java
+++ src/main/java/com/seahere/backend/auth/controller/AuthController.java
@@ -3,7 +3,11 @@ package com.seahere.backend.auth.controller;
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
 import com.seahere.backend.auth.jwt.exception.ValidateTokenException;
+import com.seahere.backend.auth.jwt.service.JwtService;
 import com.seahere.backend.auth.jwt.util.CookieUtil;
+import com.seahere.backend.auth.service.AuthService;
+import com.seahere.backend.company.response.CompanyResponse;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.coyote.Response;
 import org.springframework.beans.factory.annotation.Value;
@@ -13,17 +17,19 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
-
 import javax.servlet.http.HttpServletRequest;
 
 @Slf4j
 @RestController
+@RequiredArgsConstructor
 public class AuthController {
     @Value("${jwt.secretKey}")
     private String secretKey;
+    private final JwtService jwtService;
+    private final AuthService authService;
 
     @GetMapping("/authentication/protected")
-    public ResponseEntity<Void> authenticationTokenGet(HttpServletRequest request) {
+    public ResponseEntity<CompanyResponse> authenticationTokenGet(HttpServletRequest request) {
         String accessToken = CookieUtil.getAccessTokenFromCookies(request);
         String refreshToken = CookieUtil.getRefreshTokenFromCookie(request);
 
@@ -35,7 +41,11 @@ public class AuthController {
             headers.set("Authorization", accessToken);
             headers.set("Authorization-Refresh", refreshToken);
 
-            return new ResponseEntity<>(headers, HttpStatus.OK);
+            String email = jwtService.extractEmail(accessToken)
+                    .orElseThrow(ValidateTokenException::new);
+            CompanyResponse company = authService.getSocialUserWithCompany(email);
+
+            return new ResponseEntity<>(company, headers, HttpStatus.OK);
 
         } catch (Exception e) {
             throw new ValidateTokenException();
