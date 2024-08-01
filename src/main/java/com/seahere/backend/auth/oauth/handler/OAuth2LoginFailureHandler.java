3eea2dc58bc7c4ae1933882dd10813710778829e
kimdodo97 <eh4536@naver.com>
2024-08-02 02:05:11 +0900

    Feat : 소셜 로그인 FailHandler 예외 추가

--- src/main/java/com/seahere/backend/auth/oauth/handler/OAuth2LoginFailureHandler.java
+++ src/main/java/com/seahere/backend/auth/oauth/handler/OAuth2LoginFailureHandler.java
@@ -1,6 +1,9 @@
 package com.seahere.backend.auth.oauth.handler;
 
+import com.seahere.backend.user.exception.BrokerPermissionException;
+import com.seahere.backend.user.exception.UserNotFound;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.authentication.AuthenticationFailureHandler;
 import org.springframework.stereotype.Component;
@@ -13,10 +16,28 @@ import java.io.IOException;
 @Slf4j
 @Component
 public class OAuth2LoginFailureHandler implements AuthenticationFailureHandler {
+    @Value("${client.server.address}")
+    private String CLIENT_SERVER_ADDRESS;
+
     @Override
     public void onAuthenticationFailure(HttpServletRequest request, HttpServletResponse response, AuthenticationException exception) throws IOException, ServletException {
-        response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
-        response.getWriter().write("소셜 로그인 실패! 서버 로그를 확인해주세요.");
-        log.info("소셜 로그인에 실패했습니다. 에러 메시지 : {}", exception.getMessage());
+        response.setCharacterEncoding("UTF-8");
+        response.setContentType("application/json;charset=UTF-8");
+
+        if (exception.getCause() instanceof BrokerPermissionException) {
+            response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+            response.getWriter().write("{\"message\": \"사용자 계정이 활성화되지 않았습니다.\"}");
+            response.sendRedirect(CLIENT_SERVER_ADDRESS +"/login");
+        } else if (exception.getCause() instanceof UserNotFound) {
+            response.setStatus(HttpServletResponse.SC_NOT_FOUND);
+            response.getWriter().write("{\"message\": \"사용자를 찾을 수 없습니다.\"}");
+            response.sendRedirect(CLIENT_SERVER_ADDRESS +"/login");
+        } else {
+            response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+            response.getWriter().write("{\"message\": \"로그인 실패! 이메일이나 비밀번호를 확인해주세요.\"}");
+            response.sendRedirect(CLIENT_SERVER_ADDRESS +"/login");
+        }
+
+        log.info("로그인에 실패했습니다. 메시지 : {}", exception.getMessage());
     }
 }
