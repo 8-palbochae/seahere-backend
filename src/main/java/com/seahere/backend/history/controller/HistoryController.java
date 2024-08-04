8803d426e6eec903efdeea0b2c816308f28a0f55
Jeong gi seok <wjdrltjr5@naver.com>
2024-08-04 21:16:36 +0900

    Feat : incoming date history

--- src/main/java/com/seahere/backend/history/controller/HistoryController.java
+++ src/main/java/com/seahere/backend/history/controller/HistoryController.java
@@ -3,11 +3,11 @@ package com.seahere.backend.history.controller;
 import com.seahere.backend.history.controller.request.HistoryGetReq;
 import com.seahere.backend.history.controller.response.HistoryResponse;
 import com.seahere.backend.history.service.HistoryService;
-import com.seahere.backend.incoming.controller.request.HistoryDateRequest;
 import com.seahere.backend.incoming.controller.response.IncomingResponse;
 import com.seahere.backend.incoming.service.IncomingService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -27,9 +27,9 @@ public class HistoryController {
     private final HistoryService historyService;
 
     @GetMapping("/incomings/{date}")
-    public ResponseEntity<List<IncomingResponse>> incomingReqList(HistoryDateRequest dateRequest) {
-        log.info("이거 왜 실행 안됨??");
-        List<IncomingResponse> result = incomingService.findIncomingList(1L, dateRequest.getIncomingDate())
+    public ResponseEntity<List<IncomingResponse>> incomingReqList(@PathVariable("date") @DateTimeFormat(pattern = "yyyy-MM-dd") LocalDate date) {
+        log.info("날짜는 = {}",date);
+        List<IncomingResponse> result = incomingService.findIncomingList(1L, date)
                 .stream()
                 .map(IncomingResponse::from)
                 .collect(Collectors.toList());
