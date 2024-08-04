00bffa908d9927f2882ba2ad570d9e69d7a05a6c
kimdodo97 <eh4536@naver.com>
2024-08-04 22:51:54 +0900

    Feat : 인벤토리 수정 응답 DTO로 반환 값 변경

--- src/main/java/com/seahere/backend/inventory/service/InventoryDetailService.java
+++ src/main/java/com/seahere/backend/inventory/service/InventoryDetailService.java
@@ -1,10 +1,12 @@
 package com.seahere.backend.inventory.service;
 
 import com.seahere.backend.inventory.controller.request.InventoryEditReq;
+import com.seahere.backend.inventory.controller.response.InventorySettingRes;
 import com.seahere.backend.inventory.entity.InventoryDetailEntity;
 import com.seahere.backend.inventory.entity.InventoryEntity;
 import com.seahere.backend.inventory.exception.InventoryNotFoundException;
 import com.seahere.backend.inventory.repository.InventoryJpaRepository;
+import com.seahere.backend.user.domain.UserEntity;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -15,7 +17,7 @@ import org.springframework.transaction.annotation.Transactional;
 public class InventoryDetailService {
     private final InventoryJpaRepository inventoryJpaRepository;
 
-    public void editInventoryDetail(Long inventoryId,InventoryEditReq inventoryEditReq){
+    public InventorySettingRes editInventoryDetail(Long inventoryId, InventoryEditReq inventoryEditReq){
         InventoryEntity inventory = inventoryJpaRepository.findById(inventoryId)
                 .orElseThrow(InventoryNotFoundException::new);
 
@@ -23,5 +25,12 @@ public class InventoryDetailService {
         inventoryDetail.edit(inventoryEditReq);
 
         inventoryJpaRepository.save(inventory);
+
+        return InventorySettingRes.builder()
+                .id(inventoryDetail.getId())
+                .warningQuantity(inventoryDetail.getWarningQuantity())
+                .outgoingPrice(inventoryDetail.getOutgoingPrice())
+                .build();
+
     }
 }
