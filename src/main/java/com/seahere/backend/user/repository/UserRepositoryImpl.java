8a82ef6d26be37e6c4e5a6694c8b8570d5271c16
kimdodo97 <eh4536@naver.com>
2024-08-06 13:49:04 +0900

    Feat : 소셜 계정으로 회사 Fetch 조회 쿼리 추가

--- src/main/java/com/seahere/backend/user/repository/UserRepositoryImpl.java
+++ src/main/java/com/seahere/backend/user/repository/UserRepositoryImpl.java
@@ -1,6 +1,7 @@
 package com.seahere.backend.user.repository;
 
 import com.querydsl.jpa.impl.JPAQueryFactory;
+import com.seahere.backend.common.entity.SocialType;
 import com.seahere.backend.user.domain.QUserEntity;
 import com.seahere.backend.user.domain.UserEntity;
 import lombok.RequiredArgsConstructor;
@@ -18,6 +19,17 @@ public class UserRepositoryImpl implements UserRepositoryCustom{
         UserEntity user = jpaQueryFactory.selectFrom(QUserEntity.userEntity)
                 .leftJoin(QUserEntity.userEntity.company).fetchJoin()
                 .where(QUserEntity.userEntity.email.eq(email))
+                .fetchOne();
+
+        return Optional.ofNullable(user);
+    }
+
+    @Override
+    public Optional<UserEntity> findWithCompanyBySocialTypeAndSocialId(SocialType socialType, String SocialId) {
+        UserEntity user = jpaQueryFactory.selectFrom(QUserEntity.userEntity)
+                .leftJoin(QUserEntity.userEntity.company).fetchJoin()
+                .where(QUserEntity.userEntity.socialType.eq(socialType)
+                        .and(QUserEntity.userEntity.socialId.eq(SocialId)))
                 .fetchOne();
 
         return Optional.ofNullable(user);
