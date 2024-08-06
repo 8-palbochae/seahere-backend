d346279a91e711e853fc20e9e6545ded31290819
woneveryady <woneveryday@naver.com>
2024-08-06 14:07:59 +0900

    incomingAuthentication

--- src/main/java/com/seahere/backend/incoming/controller/IncomingController.java
+++ src/main/java/com/seahere/backend/incoming/controller/IncomingController.java
@@ -1,11 +1,14 @@
 package com.seahere.backend.incoming.controller;
 
+import com.seahere.backend.auth.login.CustomUserDetails;
 import com.seahere.backend.incoming.controller.request.IncomingDataRequest;
 import com.seahere.backend.incoming.controller.request.IncomingEditReq;
 import com.seahere.backend.incoming.service.IncomingService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -18,14 +21,11 @@ public class IncomingController {
 
     private final IncomingService incomingService;
 
-    private static final long companyId = 4L;
-    private static final long userId = 5L;
 
     @PostMapping("/incoming")
-    public ResponseEntity<String> saveIncomingData(@RequestBody IncomingDataRequest incomingDataRequest) {
-        incomingService.save(companyId, userId, incomingDataRequest);
-
-        return ResponseEntity.ok("데이터를 성공적으로 받았습니다.");
+    public ResponseEntity saveIncomingData(@RequestBody IncomingDataRequest incomingDataRequest,@AuthenticationPrincipal CustomUserDetails userDetails) {
+        incomingService.save(userDetails.getUser().getCompanyId(), userDetails.getUser().getUserId(), incomingDataRequest);
+        return  new ResponseEntity<>(HttpStatus.OK);
     }
     @PatchMapping("/incoming")
     public ResponseEntity<Long> modifyIncomingData(@RequestBody IncomingEditReq IncomingEditReq) {
