a0c7d13616a1819b6b4d831489a5c486862d964d
kimdodo97 <eh4536@naver.com>
2024-08-06 10:19:42 +0900

    Feat : 로그인 DTO 추가

---
+++ src/main/java/com/seahere/backend/common/dto/UserLogin.java
@@ -0,0 +1,44 @@
+package com.seahere.backend.common.dto;
+
+import com.seahere.backend.common.entity.Role;
+import com.seahere.backend.company.entity.CompanyEntity;
+import com.seahere.backend.user.domain.UserEntity;
+import com.seahere.backend.user.domain.UserStatus;
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+public class UserLogin {
+    private Long userId;
+    private String email;
+    private String password;
+    private Long companyId;
+    private String username;
+    private String companyName;
+    private UserStatus status;
+    private Role role;
+
+    @Builder
+    public UserLogin(Long userId, String email, String password, Long companyId, String username, String companyName, UserStatus status, Role role) {
+        this.userId = userId;
+        this.email = email;
+        this.password = password;
+        this.companyId = companyId;
+        this.username = username;
+        this.companyName = companyName;
+        this.status = status;
+        this.role = role;
+    }
+
+    public static UserLogin from(UserEntity user, CompanyEntity company){
+        return UserLogin.builder()
+                .userId(user.getId())
+                .password(user.getPassword())
+                .companyId(company.getId())
+                .companyName(company.getCompanyName())
+                .username(user.getUsername())
+                .status(user.getStatus())
+                .role(user.getRole())
+                .build();
+    }
+}
