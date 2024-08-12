800c796c672b0a5a2f5caefc57a24a6081d1fbc9
정기석(Gi Seok Jeong) <48271492+wjdrltjr5@users.noreply.github.com>
2024-08-12 09:51:15 +0900

    Feat : 출고 요청시 회사소속 유저에게 푸쉬 발송 (#108)

--- src/main/java/com/seahere/backend/alarm/service/AlarmServiceImpl.java
+++ src/main/java/com/seahere/backend/alarm/service/AlarmServiceImpl.java
@@ -2,18 +2,19 @@ package com.seahere.backend.alarm.service;
 
 import com.google.firebase.messaging.FirebaseMessaging;
 import com.google.firebase.messaging.FirebaseMessagingException;
+import com.seahere.backend.alarm.dto.AlarmToCompanyEvent;
 import com.seahere.backend.alarm.dto.AlarmToCustomerEvent;
 import com.seahere.backend.alarm.entity.AlarmHistoryEntity;
 import com.seahere.backend.alarm.entity.AlarmTokenEntity;
 import com.seahere.backend.alarm.exception.TokenNotFoundException;
 import com.seahere.backend.alarm.repository.AlarmHistoryJpaRepository;
 import com.seahere.backend.alarm.repository.AlarmJapRepository;
+import com.seahere.backend.alarm.repository.AlarmRepository;
 import com.seahere.backend.user.domain.UserEntity;
 import com.seahere.backend.user.exception.UserNotFound;
 import com.seahere.backend.user.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
@@ -32,10 +33,11 @@ public class AlarmServiceImpl implements AlarmService{
     private final AlarmHistoryJpaRepository alarmHistoryJpaRepository;
     private final AlarmJapRepository alarmJapRepository;
     private final UserRepository userRepository;
+    private final AlarmRepository alarmRepository;
 
     @Transactional(propagation = Propagation.REQUIRES_NEW)
     @TransactionalEventListener
-    public void createAlarmHistory(AlarmToCustomerEvent event) throws FirebaseMessagingException {
+    public void pushAlarmToCustomer(AlarmToCustomerEvent event) throws FirebaseMessagingException {
         log.info("이벤트 구독확인 ");
         UserEntity user = userRepository.findById(event.getUserId()).orElseThrow(UserNotFound::new);
         AlarmTokenEntity token = alarmJapRepository.findByUser(user).orElseThrow(TokenNotFoundException::new);
@@ -45,6 +47,25 @@ public class AlarmServiceImpl implements AlarmService{
                         .title(event.getTitle())
                         .body(event.getBody())
                 .build());
+    }
+
+    @Transactional(propagation = Propagation.REQUIRES_NEW)
+    @TransactionalEventListener
+    public void pushAlarmToCompanyUser(AlarmToCompanyEvent event) throws FirebaseMessagingException {
+        log.info("이벤트 구독확인 ");
+        List<AlarmTokenEntity> users = alarmRepository.findByCompanyUser(event.getCompanyId());
+        for (AlarmTokenEntity user : users) {
+            try {
+                sendMessage(user.getToken(), event.getTitle(), event.getBody());
+                alarmHistoryJpaRepository.save(AlarmHistoryEntity.builder()
+                        .userId(user.getUser().getId())
+                        .title(event.getTitle())
+                        .body(event.getBody())
+                        .build());
+            } catch (FirebaseMessagingException e) {
+                log.error("Failed to send message to user: " + user.getUser().getId(), e);
+            }
+        }
     }
 
     @Override
