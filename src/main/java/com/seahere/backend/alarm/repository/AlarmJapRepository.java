e0fe2e1f2618a9d0b9931ac5d749e55235897d43
Jeong gi seok <wjdrltjr5@naver.com>
2024-08-12 09:47:35 +0900

    Feat : 출고 요청시 회사소속 유저에게 푸쉬 발송

--- src/main/java/com/seahere/backend/alarm/repository/AlarmJapRepository.java
+++ src/main/java/com/seahere/backend/alarm/repository/AlarmJapRepository.java
@@ -1,9 +1,12 @@
 package com.seahere.backend.alarm.repository;
 
 import com.seahere.backend.alarm.entity.AlarmTokenEntity;
+import com.seahere.backend.company.entity.CompanyEntity;
 import com.seahere.backend.user.domain.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface AlarmJapRepository extends JpaRepository<AlarmTokenEntity, Long> {
