4c03d4a5ff4629077e2a5f5762680a0c5597f8a7
kimdodo97 <eh4536@naver.com>
2024-08-13 00:42:31 +0900

    Feat : 커스터머 출고 요청 상세 목록 조회 컨트롤러 맵핑 추가

--- src/main/java/com/seahere/backend/outgoing/controller/OutgoingReqController.java
+++ src/main/java/com/seahere/backend/outgoing/controller/OutgoingReqController.java
@@ -5,6 +5,7 @@ import com.seahere.backend.outgoing.controller.request.OutgoingCreateReq;
 import com.seahere.backend.outgoing.controller.request.OutgoingReqSearchRequest;
 import com.seahere.backend.outgoing.controller.request.OutgoingSearchReq;
 import com.seahere.backend.outgoing.controller.request.OutgoingStateChangeRequest;
+import com.seahere.backend.outgoing.controller.response.CustomerOutgoingDetailRes;
 import com.seahere.backend.outgoing.controller.response.OutgoingCallListResponse;
 import com.seahere.backend.outgoing.controller.response.OutgoingDetailResponse;
 import com.seahere.backend.outgoing.controller.response.OutgoingRes;
@@ -23,6 +24,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
+import javax.swing.plaf.IconUIResource;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -57,7 +59,13 @@ public class OutgoingReqController {
         return ResponseEntity.ok(outgoingResList);
     }
 
-    @GetMapping("/{outgoingId}")
+    @GetMapping("/customer/{outgoingId}")
+    public ResponseEntity<List<CustomerOutgoingDetailRes>> getOutgoingDetail(@PathVariable("outgoingId") Long outgoingId) {
+        List<CustomerOutgoingDetailRes> customerOutgoingList = outgoingDetailService.getCustomerOutgoingList(outgoingId);
+        return ResponseEntity.ok(customerOutgoingList);
+    }
+
+        @GetMapping("/{outgoingId}")
     public List<OutgoingDetailResponse> outgoingReqDetailList(@PathVariable("outgoingId") Long outgoingId){
         return outgoingDetailService.findByOutgoingAndStateIsAcitve(outgoingId).stream().map(OutgoingDetailResponse::from).collect(Collectors.toList());
     }
