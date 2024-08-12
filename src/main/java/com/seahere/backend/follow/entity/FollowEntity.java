e498f0452c5e8bcf80021276dc8b6bc27ac72379
jang643 <jang643@naver.com>
2024-08-12 11:40:05 +0900

    Feat : follow 엔티티 생성

---
+++ src/main/java/com/seahere/backend/follow/entity/FollowEntity.java
@@ -0,0 +1,31 @@
+package com.seahere.backend.follow.entity;
+
+import com.seahere.backend.company.entity.CompanyEntity;
+import com.seahere.backend.user.domain.UserEntity;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "follow")
+@Builder
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class FollowEntity {
+    @Id
+    @GeneratedValue
+    @Column(name = "follow_id")
+    private Long followId;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "company_id", nullable = false)
+    private CompanyEntity company;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "customer_id", nullable = false)
+    private UserEntity customer;
+}
