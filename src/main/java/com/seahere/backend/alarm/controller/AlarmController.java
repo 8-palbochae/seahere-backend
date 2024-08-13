460acf4d9d162873c8cfef1b7d7e418f283c9c13
Jeong gi seok <wjdrltjr5@naver.com>
2024-08-13 15:37:32 +0900

    Feat : alarm inventory

--- src/main/java/com/seahere/backend/alarm/controller/AlarmController.java
+++ src/main/java/com/seahere/backend/alarm/controller/AlarmController.java
@@ -3,12 +3,14 @@ package com.seahere.backend.alarm.controller;
 import com.seahere.backend.alarm.controller.request.TokenRequest;
 import com.seahere.backend.alarm.service.AlarmService;
 import com.seahere.backend.auth.login.CustomUserDetails;
+import com.seahere.backend.inventory.controller.response.CustomerInventoryRes;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @Slf4j
@@ -22,4 +24,7 @@ public class AlarmController {
         log.info("TokenRequest = {}, userId = {}", tokenRequest.getToken(),userDetails.getUser().getUserId());
         alarmService.saveToken(userDetails.getUser().getUserId(),tokenRequest.getToken());
     }
+
+    @GetMapping("/alarm/inventories")
+    public ResponseEntity<List<CustomerInventoryRes>> customerInventoryListGet(@PathVariable Long companyId,
 }
---
+++ src/main/java/com/seahere/backend/alarm/controller/response/InventoryRes.java
@@ -0,0 +1,45 @@
+package com.seahere.backend.alarm.controller.response;
+
+import com.seahere.backend.inventory.entity.InventoryEntity;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+
+@Getter
+@NoArgsConstructor
+public class InventoryRes {
+    private Long inventoryId;
+    private String name;
+    private String imgUrl;
+    private String category;
+    private String country;
+    private String naturalStatus;
+    private BigDecimal price;
+    private float quantity;
+    @Builder
+    public InventoryRes(Long inventoryId, String name, String imgUrl, String category, String country, String naturalStatus, BigDecimal price, float quantity) {
+        this.inventoryId = inventoryId;
+        this.name = name;
+        this.imgUrl = imgUrl;
+        this.category = category;
+        this.country = country;
+        this.naturalStatus = naturalStatus;
+        this.price = price;
+        this.quantity = quantity;
+    }
+
+    public static InventoryRes from(InventoryEntity inventory){
+        return InventoryRes.builder()
+                .inventoryId(inventory.getInventoryId())
+                .name(inventory.getProduct().getProductName())
+                .imgUrl(inventory.getProduct().getProductImg())
+                .price(inventory.getInventoryDetail().getOutgoingPrice())
+                .country(inventory.getCountry())
+                .category(inventory.getCategory())
+                .naturalStatus(inventory.getNaturalStatus())
+                .quantity(inventory.getQuantity())
+                .build();
+    }
+}
