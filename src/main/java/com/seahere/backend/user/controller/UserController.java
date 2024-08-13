7f0d5445f45b1aa6f8600cd1c85908edbc77cbe5
DOYOUNG KIM <82021950+kimdodo97@users.noreply.github.com>
2024-08-13 17:16:05 +0900

    Feat : 사용자 정보 조회 기능 구현 (#118)
    * Feat : 사용자 정보 조회 응답 DTO
    
    * Feat : 사용자 정보 조회 서비스 로직 추가
    
    * Feat : 로그인된 유저 정보 조회 컨트롤러 맵핑 추가

--- src/main/java/com/seahere/backend/user/controller/UserController.java
+++ src/main/java/com/seahere/backend/user/controller/UserController.java
@@ -1,5 +1,8 @@
 package com.seahere.backend.user.controller;
 
+import com.google.firebase.auth.UserInfo;
+import com.seahere.backend.auth.login.CustomUserDetails;
+import com.seahere.backend.user.controller.response.UserInfoRes;
 import com.seahere.backend.user.exception.DuplicateEmailException;
 import com.seahere.backend.user.request.BrokerSignupReq;
 import com.seahere.backend.user.request.CeoSignupReq;
@@ -8,6 +11,7 @@ import com.seahere.backend.user.request.OAuthSignupReq;
 import com.seahere.backend.user.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -68,5 +72,11 @@ public class UserController {
         response.put("message", isAvailable ? "이메일을 사용할 수 있습니다." : "이메일이 이미 사용 중입니다.");
 
         return ResponseEntity.ok(response);
+    }
+
+    @GetMapping("/users/myinfo")
+    public ResponseEntity<UserInfoRes> getMyInfo(@AuthenticationPrincipal CustomUserDetails userDetails){
+        UserInfoRes user = userService.getUser(userDetails.getUser().getUserId());
+        return ResponseEntity.ok(user);
     }
 }
