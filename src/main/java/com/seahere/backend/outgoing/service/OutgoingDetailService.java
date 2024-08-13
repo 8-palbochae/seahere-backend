b2fb36624595edd9eafe803d56329016d8817f60
DOYOUNG KIM <82021950+kimdodo97@users.noreply.github.com>
2024-08-13 09:34:16 +0900

    Feat : 커스터머 전용 출고 내역 상세 조회 기능 구현 (#114)
    * Feat : 커스터머 출고 요청 목록 조회 DTO 추가
    
    * Feat : 커스터머 출고 요청 상세 목록 조회 쿼리 DSL 추가
    
    * Feat : 커스터머 출고 요청 상세 목록 조회 서비스 로직 추가
    
    * Feat : 커스터머 출고 요청 상세 목록 조회 컨트롤러 맵핑 추가

--- src/main/java/com/seahere/backend/outgoing/service/OutgoingDetailService.java
+++ src/main/java/com/seahere/backend/outgoing/service/OutgoingDetailService.java
@@ -1,5 +1,6 @@
 package com.seahere.backend.outgoing.service;
 
+import com.seahere.backend.outgoing.controller.response.CustomerOutgoingDetailRes;
 import com.seahere.backend.outgoing.dto.OutgoingDetailDto;
 import com.seahere.backend.outgoing.entity.OutgoingDetailEntity;
 import com.seahere.backend.outgoing.entity.OutgoingDetailState;
@@ -16,6 +17,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
@@ -46,5 +48,12 @@ public class OutgoingDetailService {
     public void updateByOutgoingDetailStateToActive(Long outgoingId){
         OutgoingEntity outgoing = outgoingJpaRepository.findById(outgoingId).orElseThrow(OutgoingNotFoundException::new);
         outgoingDetailJpaRepository.updateByOutgoingDetailStateToActive(outgoing,OutgoingDetailState.ACTIVE);
+    }
+
+    public List<CustomerOutgoingDetailRes> getCustomerOutgoingList(Long outgoingId){
+        return outgoingDetailRepository.findByOutgoingId(outgoingId)
+                .stream()
+                .map(CustomerOutgoingDetailRes::from)
+                .collect(Collectors.toList());
     }
 }
