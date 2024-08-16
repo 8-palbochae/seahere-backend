a25e9dc12622e15616830fb00cc6788e500cbca5
jang643 <jang643@naver.com>
2024-08-16 16:59:46 +0900

    Refact : Ocr 시크릿키 리팩토링

--- src/main/java/com/seahere/backend/ocr/repository/OcrRepository.java
+++ src/main/java/com/seahere/backend/ocr/repository/OcrRepository.java
@@ -2,6 +2,9 @@ package com.seahere.backend.ocr.repository;
 
 import com.seahere.backend.ocr.controller.request.MultipartFileRequestBody;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
@@ -11,20 +14,22 @@ import org.springframework.web.client.RestTemplate;
 
 @Repository
 @Slf4j
+@PropertySource("classpath:ocrConfig.properties")
 public class OcrRepository {
 
-    // RestTemplate을 직접 인스턴스화
     private final RestTemplate restTemplate = new RestTemplate();
 
-    private static final String CLOVA_OCR_API_URL = "https://7n6f2iipra.apigw.ntruss.com/custom/v1/33393/ea7766b1bdfaafa82caf417d026f4c7c270112e21f9f70860d596f42f8dcc17c/document/biz-license";
-    private static final String CLOVA_OCR_SECRET_KEY = "RUducW9Id1BWU1hjTVpOU0lYQXpZY3NlVHFxc1BuSXk=";  // 실제 시크릿 키로 변경
+    @Autowired
+    private Environment env;
 
     public String callOcrApi(byte[] imageBytes) {
+        String clovaOcrApiUrl = env.getProperty("clova.ocr.api.url");
+        String clovaOcrSecretKey = env.getProperty("clova.ocr.secret.key");
+
         HttpHeaders headers = new HttpHeaders();
         headers.set("Content-Type", "multipart/form-data");
-        headers.set("X-OCR-SECRET", CLOVA_OCR_SECRET_KEY);
+        headers.set("X-OCR-SECRET", clovaOcrSecretKey);
 
-        // JSON 메시지 구성
         String jsonMessage = "{\n" +
                 "    \"version\": \"V2\",\n" +
                 "    \"requestId\": \"uuid\",\n" +
@@ -37,13 +42,11 @@ public class OcrRepository {
                 "    ]\n" +
                 "}";
 
-        // 멀티파트 요청 구성
         MultipartFileRequestBody multipartBody = new MultipartFileRequestBody(imageBytes, jsonMessage);
         HttpEntity<MultipartFileRequestBody> requestEntity = new HttpEntity<>(multipartBody, headers);
 
-        // 클로바 OCR API 호출
         ResponseEntity<String> response = restTemplate.exchange(
-                CLOVA_OCR_API_URL, HttpMethod.POST, requestEntity, String.class);
+                clovaOcrApiUrl, HttpMethod.POST, requestEntity, String.class);
 
         return response.getBody();
     }
