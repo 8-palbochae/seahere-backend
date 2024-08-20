28913d2d7e35aec00500ea73358980142d39ac31
Jeong gi seok <wjdrltjr5@naver.com>
2024-08-20 15:56:48 +0900

    Feat : 직원 추가

--- src/main/java/com/seahere/backend/user/controller/TeamController.java
+++ src/main/java/com/seahere/backend/user/controller/TeamController.java
@@ -1,21 +1,27 @@
 package com.seahere.backend.user.controller;
 
+import com.seahere.backend.auth.login.CustomUserDetails;
+import com.seahere.backend.user.controller.request.EmployeeAddRequest;
 import com.seahere.backend.user.service.UserService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
 import java.security.Principal;
 
 @RestController
 @RequiredArgsConstructor
+@Slf4j
 public class TeamController {
     private final UserService userService;
 
     @PatchMapping("/teams")
-    public ResponseEntity<Void> memberApprove(@RequestBody String employeeEmail, Principal principal){
-        String loginEmail = principal.getName();
-        userService.approveEmployee(loginEmail,employeeEmail);
+    public ResponseEntity<Void> memberApprove(@RequestBody EmployeeAddRequest employeeEmail, @AuthenticationPrincipal CustomUserDetails userDetails){
+        log.info("employeeEmail = {}, principal = {}", employeeEmail, userDetails.getUser().getEmail());
+        String loginEmail = userDetails.getUser().getEmail();
+        userService.approveEmployee(loginEmail,employeeEmail.getEmployeeEmail());
 
         return ResponseEntity.ok(null);
     }
