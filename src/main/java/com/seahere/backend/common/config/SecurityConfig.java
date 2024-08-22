e951155e179f01f64a084440b824b584f6a9dc68
kimdodo97 <eh4536@naver.com>
2024-08-22 15:18:03 +0900

    Feat : 시큐리티 로그아웃 추가 구현

--- src/main/java/com/seahere/backend/common/config/SecurityConfig.java
+++ src/main/java/com/seahere/backend/common/config/SecurityConfig.java
@@ -5,17 +5,20 @@ import com.seahere.backend.auth.filter.CustomClientBranchFilter;
 import com.seahere.backend.auth.jwt.filter.JwtAuthenticationProcessingFilter;
 import com.seahere.backend.auth.jwt.service.JwtService;
 import com.seahere.backend.auth.login.filter.CustomJsonUsernamePasswordAuthenticationFilter;
+import com.seahere.backend.auth.login.handler.CustomLogoutSuccessHandler;
 import com.seahere.backend.auth.login.handler.LoginFailureHandler;
 import com.seahere.backend.auth.login.handler.LoginSuccessHandler;
 import com.seahere.backend.auth.login.service.LoginService;
 import com.seahere.backend.auth.oauth.handler.OAuth2LoginFailureHandler;
 import com.seahere.backend.auth.oauth.handler.OAuth2LoginSuccessHandler;
 import com.seahere.backend.auth.oauth.service.CustomOAuth2UserService;
+import com.seahere.backend.redis.respository.TokenRepository;
 import com.seahere.backend.user.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.ProviderManager;
@@ -42,6 +45,9 @@ public class SecurityConfig implements WebMvcConfigurer {
     private final OAuth2LoginSuccessHandler oAuth2LoginSuccessHandler;
     private final OAuth2LoginFailureHandler oAuth2LoginFailureHandler;
     private final CustomOAuth2UserService customOAuth2UserService;
+    private final TokenRepository tokenRepository;
+    private final CustomLogoutSuccessHandler customLogoutSuccessHandler;
+
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http, ServletRegistrationBean h2Console) throws Exception {
          http
@@ -64,12 +70,17 @@ public class SecurityConfig implements WebMvcConfigurer {
                  .anyRequest().authenticated()
                  .and()
             .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-            .and()
+                 .and()
+                 .logout()
+                 .logoutUrl("/logout")  // 로그아웃 요청을 받을 URL
+                .logoutSuccessHandler(logoutSuccessHandler())  // 커스텀 로그아웃 성공 핸들러 설정
+                .deleteCookies("JSESSIONID")  // 쿠키 삭제
+                .invalidateHttpSession(true)  // 세션 무효화
+                .and()
             .oauth2Login()
             .successHandler(oAuth2LoginSuccessHandler)
             .failureHandler(oAuth2LoginFailureHandler)
             .userInfoEndpoint().userService(customOAuth2UserService);
-
          http.addFilterBefore(customClientFilter(), OAuth2AuthorizationRequestRedirectFilter.class);
          http.addFilterAfter(customJsonUsernamePasswordAuthenticationFilter(), LogoutFilter.class);
          http.addFilterBefore(jwtAuthenticationProcessingFilter(), CustomJsonUsernamePasswordAuthenticationFilter.class);
@@ -111,12 +122,17 @@ public class SecurityConfig implements WebMvcConfigurer {
 
     @Bean
     public LoginSuccessHandler loginSuccessHandler() {
-        return new LoginSuccessHandler(jwtService, userRepository);
+        return new LoginSuccessHandler(jwtService, userRepository,tokenRepository);
     }
 
     @Bean
     public LoginFailureHandler loginFailureHandler() {
         return new LoginFailureHandler();
+    }
+
+    @Bean
+    public CustomLogoutSuccessHandler logoutSuccessHandler() {
+        return new CustomLogoutSuccessHandler(tokenRepository);
     }
 
     @Bean
@@ -137,7 +153,7 @@ public class SecurityConfig implements WebMvcConfigurer {
 
     @Bean
     public JwtAuthenticationProcessingFilter jwtAuthenticationProcessingFilter() {
-        JwtAuthenticationProcessingFilter jwtAuthenticationFilter = new JwtAuthenticationProcessingFilter(jwtService, userRepository);
+        JwtAuthenticationProcessingFilter jwtAuthenticationFilter = new JwtAuthenticationProcessingFilter(jwtService, userRepository,tokenRepository    );
         return jwtAuthenticationFilter;
     }
 }
